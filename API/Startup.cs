bc2c8a972408f672a99070faaa090e0300a307fc
Chaitanya Lotake <lotakec@gmail.com>
2022-09-01 14:26:25 +0530

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,45 +3,52 @@ using Microsoft.EntityFrameworkCore;
 using API.Helpers;
 using API.Middleware;
 using API.Extensions;
-
+using StackExchange.Redis;
 namespace API
 {
     public class Startup
     {
-    
+
         private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
-        {     
+        {
             _config = config;
         }
 
-      
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x=> 
+            services.AddDbContext<StoreContext>(x =>
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuraion = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),
+                true);
+                return ConnectionMultiplexer.Connect(configuraion);
+            });
             services.AddApplicationServices();
-           services.AddSwaggerDocumentation();
-           services.AddCors(options=>
-           {
-                options.AddPolicy("CorsPolicy",policy=>{
+            services.AddSwaggerDocumentation();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
-           });
-           
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            
-            
+
+
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
 
