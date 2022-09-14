ff194c12affdc58491dab84eb67466d7bdea42ee
Chaitanya Lotake <lotakec@gmail.com>
2022-09-14 11:36:10 +0530

    End of section 19

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Basket, IBasket, IBasketItem, IBasketTotals } from '../shared/models/basket';
+import { IDeliveryMethod } from '../shared/models/delieveryMethod';
 import { IProduct } from '../shared/models/product';
 
 @Injectable({
@@ -16,9 +17,15 @@ export class BasketService {
 
   private basketTotalSource= new BehaviorSubject<IBasketTotals>(null);
   basketTotal$=this.basketTotalSource.asObservable();
+  shipping=0;
 
   constructor(private http: HttpClient) {
 
+  }
+  setShippingPrice(deliveryMethod:IDeliveryMethod)
+  {
+    this.shipping=deliveryMethod.price;
+    this.calculateTotals();
   }
 
   getBasket(id: string) {
@@ -91,6 +98,13 @@ export class BasketService {
    }
   }
 
+  deleteLocalBasket(id:string)
+  {
+    this.basketSource.next(null);
+    this.basketTotalSource.next(null);
+    localStorage.removeItem('basket_id');
+  }
+
   deleteBasket(basket: IBasket) {
     return this.http.delete(this.baseUrl+'basket?id'+basket.id).subscribe(()=>{
       this.basketSource.next(null);
@@ -106,7 +120,7 @@ export class BasketService {
   private calculateTotals()
   {
     const basket=this.getCurrentBasketValue();
-    const shipping=0;
+    const shipping=this.shipping;
     const subtotal=basket.items.reduce((a,b)=>(b.price*b.quantity)+a,0);
     const total=subtotal+shipping;
     this.basketTotalSource.next({
